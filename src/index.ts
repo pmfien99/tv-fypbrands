4fb6b069a4401ef5007e354d29176e9b4852d908
Patrick Fien <pmfien99@icloud.com>
2023-08-11 11:26:59 -0400

    install gsap

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+import { gsap } from 'gsap';
+import { InertiaPlugin } from 'gsap/InertiaPlugin';
+
 import { greetUser } from '$utils/greet';
+
+function update() {
+  gsap.registerPlugin(InertiaPlugin);
+
+  console.log('Creating dragable items');
+  Draggable.create('.sticker', {
+    intertia: true,
+  });
+}
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
+  update();
 });
